dd0b5d271d6c8a74cb33627c8ef5fca7850137f8
Tarun Gusain <36428256+tarunngusain08@users.noreply.github.com>
2024-02-03 18:02:05 +0530

    Update main.go

--- main.go
+++ main.go
@@ -11,4 +11,9 @@ func main() {
 	auth := api.Group("/auth")
 	auth.POST("/signup", handler.SignUpHandler.SignUp)
 	auth.POST("/login", handler.LoginHandler.Login)
+
+	notes := api.Group("/notes")
+	notes.POST("")
+	notes.GET("/:id", handler.GetNotesHandler.GetNotesById)
+	notes.GET("", handler.GetNotesHandler.GetAllNotes)
 }
