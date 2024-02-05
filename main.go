f9e77dac9a6ac90036fc7886b7979b5ad190ec21
Tarun Gusain <36428256+tarunngusain08@users.noreply.github.com>
2024-02-05 18:07:43 +0530

    Update main.go

--- main.go
+++ main.go
@@ -2,18 +2,32 @@ package main
 
 import (
 	"github.com/gin-gonic/gin"
+	"github.com/juju/ratelimit"
 )
 
 func main() {
 	r := gin.Default()
+
+	limiter := ratelimit.NewBucketWithRate(100, 100)
+	limiter.Capacity()
+
+	r.Use(func(c *gin.Context) {
+		limiter.Wait(1)
+		c.Next()
+	})
+
 	api := r.Group("/api")
 
 	auth := api.Group("/auth")
-	auth.POST("/signup", handler.SignUpHandler.SignUp)
-	auth.POST("/login", handler.LoginHandler.Login)
+	{
+		auth.POST("/signup", handler.SignUpHandler.SignUp)
+		auth.POST("/login", handler.LoginHandler.Login)
+	}
 
 	notes := api.Group("/notes")
-	notes.POST("")
-	notes.GET("/:id", handler.GetNotesHandler.GetNotesById)
-	notes.GET("", handler.GetNotesHandler.GetAllNotes)
+	{
+		notes.POST("")
+		notes.GET("/:id", handler.GetNotesHandler.GetNotesById)
+		notes.GET("", handler.GetNotesHandler.GetAllNotes)
+	}
 }
