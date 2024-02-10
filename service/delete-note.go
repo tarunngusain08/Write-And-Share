5b5c4a4a9b5273829b4410127c07c006466570aa
Tarun Gusain <36428256+tarunngusain08@users.noreply.github.com>
2024-02-10 16:57:43 +0530

    Create delete-note.go

---
+++ service/delete-note.go
@@ -0,0 +1,18 @@
+package service
+
+import (
+	"Write-And-Share/contracts"
+	"Write-And-Share/repo"
+)
+
+type DeleteNoteService struct {
+	repo *repo.DeleteNoteRepo
+}
+
+func NewDeleteNoteRepo(notesRepo *repo.DeleteNoteRepo) *DeleteNoteService {
+	return &DeleteNoteService{notesRepo}
+}
+
+func (g *DeleteNoteService) DeleteNote(request *contracts.DeleteNoteRequest) error {
+	return g.repo.DeleteNote(request)
+}
