eb055d5a3bb7e8003d1d12eeb02f367457297405
Tarun Gusain <36428256+tarunngusain08@users.noreply.github.com>
2024-02-11 17:37:21 +0530

    Update search-note.go

--- service/search-note.go
+++ service/search-note.go
@@ -3,7 +3,6 @@ package service
 import (
 	"Write-And-Share/contracts"
 	"Write-And-Share/repo"
-	"strings"
 )
 
 type SearchNoteService struct {
@@ -15,7 +14,5 @@ func NewSearchNoteService(notesRepo *repo.SearchNoteRepo) *SearchNoteService {
 }
 
 func (g *SearchNoteService) SearchNote(request *contracts.SearchNoteRequest) (*contracts.NotesList, error) {
-
-	request.Keywords = strings.Join(strings.Split(request.Keywords, " "), " | ")
 	return g.repo.SearchNote(request)
 }
