175c7e4f7d70ad7a01a89bdb48a687d849b4678c
Tarun Gusain <36428256+tarunngusain08@users.noreply.github.com>
2024-02-11 17:23:50 +0530

    Update get-notes.go

--- repo/get-notes.go
+++ repo/get-notes.go
@@ -14,25 +14,29 @@ func NewGetNotesRepo(db *sql.DB) *GetNotesRepo {
 	return &GetNotesRepo{db: db}
 }
 
-const getAllNotes = `SELECT title, content FROM notes_to_user_mapping n WHERE user_name = $1;`
-
 const getNoteById = `SELECT title, content FROM notes_to_user_mapping WHERE user_name = $1 AND note_id = $2;`
 
-func (g *GetNotesRepo) GetAllNotes(request *contracts.GetNotesRequest) (*contracts.GetNotesResponse, error) {
+const getAllNotes = `SELECT title, content FROM notes_to_user_mapping n WHERE user_name = $1 LIMIT $2 OFFSET $3;`
 
-	response := make(contracts.GetNotesResponse, 0)
-	rows, err := g.db.Query(getAllNotes, request.UserName)
+func (g *GetNotesRepo) GetAllNotes(request *contracts.GetNotesRequest) (*contracts.NotesList, error) {
+
+	page := *request.PageNum
+	pageSize := *request.PageSize
+	offset := (page - 1) * pageSize
+
+	response := make(contracts.NotesList, 0)
+	rows, err := g.db.Query(getAllNotes, request.UserName, pageSize, offset)
 	if err != nil {
 		return nil, fmt.Errorf("failed to fetch notes: %v", err)
 	}
 	defer rows.Close()
 
 	for rows.Next() {
-		var note contracts.Note
+		var note *contracts.Note
 		if err := rows.Scan(&note.Id, &note.Title, &note.Content); err != nil {
 			return nil, fmt.Errorf("failed to scan note: %v", err)
 		}
-		response = append(response, &note)
+		response = append(response, note)
 	}
 
 	if err := rows.Err(); err != nil {
@@ -41,10 +45,9 @@ func (g *GetNotesRepo) GetAllNotes(request *contracts.GetNotesRequest) (*contrac
 
 	return &response, nil
 }
+func (g *GetNotesRepo) GetNoteById(request *contracts.GetNotesRequest) (*contracts.NotesList, error) {
 
-func (g *GetNotesRepo) GetNoteById(request *contracts.GetNotesRequest) (*contracts.GetNotesResponse, error) {
-
-	response := make(contracts.GetNotesResponse, 0)
+	response := make(contracts.NotesList, 0)
 	rows, err := g.db.Query(getNoteById, request.UserName, request.NoteId)
 	if err != nil {
 		return nil, fmt.Errorf("failed to fetch notes: %v", err)
