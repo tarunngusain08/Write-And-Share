8ba649b21bc22ba5507f3db84fe6d4fdfe2d5b8e
Tarun Gusain <36428256+tarunngusain08@users.noreply.github.com>
2024-02-11 17:24:09 +0530

    Create search-note.go

---
+++ repo/search-note.go
@@ -0,0 +1,39 @@
+package repo
+
+import (
+	"Write-And-Share/contracts"
+	"database/sql"
+)
+
+type SearchNoteRepo struct {
+	db *sql.DB
+}
+
+func NewSearchNoteRepo(db *sql.DB) *SearchNoteRepo {
+	return &SearchNoteRepo{db}
+}
+
+const searchNotes = `SELECT id, title FROM notes
+WHERE to_tsvector('english', title) @@ to_tsquery('english', $1)
+ORDER BY timestamp DESC
+LIMIT $2 OFFSET $3;`
+
+func (l *SearchNoteRepo) SearchNote(request *contracts.SearchNoteRequest) (*contracts.NotesList, error) {
+	page := *request.PageNum
+	pageSize := *request.PageSize
+	offset := (page - 1) * pageSize
+	res, err := l.db.Query(searchNotes, request.Keywords, pageSize, offset)
+	if err != nil {
+		return nil, err
+	}
+
+	response := make(contracts.NotesList, 0)
+	for res.Next() {
+		var note *contracts.Note
+		if err := res.Scan(&note.Id, &note.Title); err != nil {
+			return nil, err
+		}
+		response = append(response, note)
+	}
+	return &response, nil
+}
